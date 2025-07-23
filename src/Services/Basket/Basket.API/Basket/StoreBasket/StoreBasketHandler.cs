5ce0b6af152ec7f2b6b294ee27df09c411a778e9
Kukuh Setyo Raharjo <KukuhSetyo.Raharjo@infineon.com>
2025-07-23 10:06:32 +0700

    8-123_Develop Basket Features in CQRS Handler classes using BasketRepository

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -12,7 +12,7 @@
         }
     }
 
-    public class StoreBasketCommandHandler
+    public class StoreBasketCommandHandler(IBasketRepository repository)
         : ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
@@ -21,8 +21,9 @@
 
             //TODO: store basket in database (use Marten upsert - if exist = update, if not = insert)
             //TODO: update cache
+            await repository.StoreBasket(command.Cart, cancellationToken);
 
-            return new StoreBasketResult("swn");
+            return new StoreBasketResult(command.Cart.UserName);
         }
     }
 }
