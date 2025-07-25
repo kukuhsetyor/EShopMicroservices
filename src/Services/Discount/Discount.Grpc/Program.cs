639646101c9183050ce593c5783d6b02cf3ddc68
Kukuh Setyo Raharjo <KukuhSetyo.Raharjo@infineon.com>
2025-07-25 09:56:20 +0700

    11-152_Develop discount.proto ProtoBuf file for Exposing Crud Services in Discount Grpc

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,5 +1,3 @@
-using Discount.Grpc.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,7 +10,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
     app.MapGrpcReflectionService();
 
-app.MapGrpcService<GreeterService>();
+//app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
